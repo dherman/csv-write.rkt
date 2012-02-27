1e3c82dfd20c88a9f6b776e25f7f2bc50c0d3229
Dave Herman <dherman@mozilla.com>
2012-02-26 20:36:48 -0800

    created

---
+++ README.md
@@ -0,0 +1,9 @@
+## csv-write.rkt
+
+Racket utilities for writing CSV files.
+
+## License
+
+Copyright © 2012 Dave Herman
+
+Licensed under the [MIT License](http://mit-license.org).
---
+++ info.rkt
@@ -0,0 +1,11 @@
+#lang setup/infotab
+(define name "csv-write")
+(define blurb
+  (list "Generates CSV-formatted output."))
+(define primary-file "main.rkt")
+(define doc.txt "doc.txt")
+(define version "2")
+(define release-notes
+  (list '(p "Updated for Racket.")))
+(define required-core-version "5.0")
+(define repositories '("4.x"))
